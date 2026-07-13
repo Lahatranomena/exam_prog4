35e926cb1c0e6b740637e963eb9544f92f54ec81
Lahatranomena <hei.lahatra.3@gmail.com>
2026-07-13 08:56:07 +0300

    feat(submission): add controller with submit and getAll endpoints

---
+++ src/main/java/com/exam/demo/endpoint/rest/controller/health/SubmissionController.java
@@ -0,0 +1,25 @@
+package com.exam.demo.endpoint.rest.controller.health;
+
+import com.exam.demo.service.event.SubmissionService;
+import com.exam.demo.submission.entity.Submission;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/submissions")
+public class SubmissionController {
+  private final SubmissionService service;
+
+  @PostMapping
+  public Submission submit(@RequestParam String email, @RequestParam MultipartFile file) {
+    return service.submit(email, file);
+  }
+
+  @GetMapping
+  public List<Submission> getAll() {
+    return service.getAll();
+  }
+}
