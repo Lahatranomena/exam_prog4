6ad1b914c7e6f411400394cba43129714538f2c9
Lahatranomena <hei.lahatra.3@gmail.com>
2026-07-13 09:58:06 +0300

    feat(test): add SubmissionServiceTest et move SubmissionController

---
+++ src/main/java/com/exam/demo/submission/controller/SubmissionController.java
@@ -0,0 +1,25 @@
+package com.exam.demo.submission.controller;
+
+import com.exam.demo.submission.entity.Submission;
+import com.exam.demo.submission.service.SubmissionService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/submissions")
+public class SubmissionController {
+  private final SubmissionService service;
+
+  @PostMapping
+  public Submission submit(@RequestParam String email, @RequestParam MultipartFile file) {
+    return service.submit(email, file);
+  }
+
+  @GetMapping
+  public List<Submission> getAll() {
+    return service.getAll();
+  }
+}
